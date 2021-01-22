02aa8d4157888a50dfcf8d70c86932a3644a02b7
arditsulce <europe.gov.eu@gmail.com>
2021-01-22 15:07:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# What are these files?
+These are the files of a Python program (a.k.a. App 1) built in the Udemy course  "The Complete Python Course in the Professional OOP Approach."
+The course covers how to make this program step by step, plus nine other Python programs. 
+If you want to learn Python, you can take the course for a high discount in the link below: 
+https://www.udemy.com/course/the-python-pro-course/?referralCode=D1224FDF916B73D7E740
+# What does the program do?
+The program is written in Python. When the program is executed, an HTML file is generated. The file contains a Leaflet web map with multiple clickable point markers. 
+When the user clicks on a marker, a popup window shows up displaying 
+the weather forecast for the marker location for the next 12 hours.
+# How to run the program
+1. Either clone this repo or download all these files by going to _Code -> Download ZIP_.
+2. Create a PyCharm (or other IDE) project and configure a Python interpreter for the project.
+3. Open the terminal and install the required packages by running:
+   `pip install -r requirements.txt`
+4. Run _main.py_ (the one located in the project root directory) with Python.
+5. Open http://127.0.0.1:5000/ on your internet browser to see the web app.
---
+++ main.py
@@ -0,0 +1,59 @@
+from flask.views import MethodView
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
+from flatmates_bill import flat
+
+app = Flask(__name__)
+
+class HomePage(MethodView):
+
+    def get(self):
+        return render_template('index.html')
+
+
+class BillFormPage(MethodView):
+
+    def get(self):
+        bill_form = BillForm()
+        return render_template('bill_form_page.html',
+                               billform=bill_form)
+
+
+class ResultsPage(MethodView):
+
+    def post(self):
+        billform = BillForm(request.form)
+
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+
+        return render_template('results.html',
+                               name1=flatmate1.name,
+                               amount1=flatmate1.pays(the_bill, flatmate2),
+                               name2=flatmate2.name,
+                               amount2=flatmate2.pays(the_bill, flatmate1))
+
+
+class BillForm(Form):
+    amount = StringField("Bill Amount: ", default="100")
+    period = StringField("Bill Period: ", default="December 2020")
+
+    name1 = StringField("Name: ", default="John")
+    days_in_house1 = StringField("Days in the house: ", default=20)
+
+    name2 = StringField("Name: ", default="Marry")
+    days_in_house2 = StringField("Days in the house: ", default=12)
+
+    button = SubmitField("Calculate")
+
+
+app.add_url_rule('/',
+                 view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill_form',
+                 view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/results',
+                 view_func=ResultsPage.as_view('results_page'))
+
+app.run(debug=True)
